d0f05707308ab492fd0f463b311a373cd0c7a64f
DatBItch1377 <110708421+DatBItch1377@users.noreply.github.com>
2025-05-11 18:09:55 -0500

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,92 @@
+//JavaScript Program to Convert Kilometers to Miles
+//We know that 1 kilometer is equal to 0.621371 miles.
+//So to convert kilometers to miles, you can use the formula:
+
+// taking kilometers input from the user
+const kilometers = prompt("Enter value in kilometers: ")
+
+// conversion factor
+const factor = 0.621371
+
+// calculate miles
+const miles = kilometers * factor
+
+console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
+
+
+// generating  a random number
+const a = Math.random();
+console.log(a);
+
+stat
+
+// program to find the factorial. func
+
+// take input from the user
+const number = parseInt(prompt('Enter a positive integer: '));
+
+//test aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa fu
+// checking if number is negative
+if (number < 0) {
+    console.log('Error! Factorial for negative number does not exist.');
+}
+
+// if number is 0
+else if (number === 0) {
+    console.log(`The factorial of ${number} is 1.`);
+}
+
+// if number is positive
+else {
+    let fact = 1;
+    for (i = 1; i <= number; i++) {
+        fact *= i;
+    }
+    console.log(`The factorial of ${number} is ${fact}.`);
+}
+
+
+// program to shuffle the deck of cards
+// declare card elements
+const suits = ["Spades", "Diamonds", "Club", "Heart"];
+const values = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+
+// empty array to contain cards
+let deck = [];
+
+// create a deck of cards
+for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+        let card = { Value: values[x], Suit: suits[i] };
+        deck.push(card);
+    }
+}
+
+// shuffle the cards
+for (let i = deck.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+}
+
+console.log('The first five cards are:');
+
+// display 5 results
+for (let i = 0; i < 5; i++) {
+    console.log(`${deck[i].Value} of ${deck[i].Suit}`)
+}
